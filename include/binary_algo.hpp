18c3b29a535f0452551149be9adeb59d0fbf5440
Maxim <maxim1998g@gmail.com>
2019-09-29 22:03:48 +0300

    Print more info for first task

--- include/binary_algo.hpp
+++ include/binary_algo.hpp
@@ -9,11 +9,19 @@ namespace binary_algo
 
     dfa::automaton do_act(
         const dfa::automaton& a, const dfa::automaton& b, 
-        OPERATOR op
+        OPERATOR op,
+        bool pair = true
     );
 
     namespace
     {
+        void print_pairs(
+            const std::vector<std::pair<uint32_t, uint32_t>> &states,
+            const std::pair<uint32_t, uint32_t>& start, 
+            const std::vector<std::pair<uint32_t, uint32_t>> &final_states,
+            const std::vector<std::pair<uint32_t, uint32_t>> &table
+        );
+
         bool act_condition(bool a, bool b, OPERATOR op);
     }
 }
